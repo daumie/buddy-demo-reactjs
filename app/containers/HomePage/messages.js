71f2b9c6384e47ed9733b5dd6421737d468d773b
Dominic Motuka <dominic.motuka@gmail.com>
2020-01-29 17:14:01 +0300

    Change Main Header message
    Signed-off-by: Dominic Motuka <dominic.motuka@gmail.com>

--- app/containers/HomePage/messages.js
+++ app/containers/HomePage/messages.js
@@ -10,7 +10,7 @@ export const scope = 'boilerplate.containers.HomePage';
 export default defineMessages({
   startProjectHeader: {
     id: `${scope}.start_project.header`,
-    defaultMessage: 'Start your next react project in seconds',
+    defaultMessage: 'Buddy CI/CD Demo for a Highly Scalable ReactJS Frontend',
   },
   startProjectMessage: {
     id: `${scope}.start_project.message`,
